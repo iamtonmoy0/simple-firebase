c17828505757d0e5ff8e394e958a9457c3de2064
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-17 02:12:14 -0400

    feat: signin and signout functionality created

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,50 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+	const [data,setData]=useState()
 	const auth=getAuth(app);
 	const provider=new GoogleAuthProvider();
 	const handleGoogle=()=>{
 		signInWithPopup(auth,provider)
 		.then(result=>{
 			const user= result.user;
-			console.log(user)
+			setData(user)
+		
+			
+			
 		})
 		.catch(error=>{
 			console.log('error',error.message)
 		})
 	}
+	console.log(data)
+	const handleLogout=()=>{
+		signOut(auth)
+		.then(result=>{
+			console.log(result)
+			setData(null)
+		})
+		.catch(error=>{
+			console.log(error)
+		})
+	}
+
+	
 	return (
 		<div>
-			
+			{data?
+			<button onClick={handleLogout}>logout</button>:
 			<button onClick={handleGoogle}>Google login</button>
+			}
+
+                          <h3>name{data.displayName} </h3>
+			  <p>{data.email}</p>     
+
+
 		</div>
+		
 	);
 }
 
