fce89ad55d81fa774b1251fc67bebc7f680843af
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-17 12:36:13 -0400

    feat: github auth added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/firebase.init";
 import { useState } from "react";
 
@@ -6,6 +6,7 @@ const Login = () => {
 	const [data,setData]=useState()
 	const auth=getAuth(app);
 	const provider=new GoogleAuthProvider();
+	const gitProvider=new GithubAuthProvider();
 	const handleGoogle=()=>{
 		signInWithPopup(auth,provider)
 		.then(result=>{
@@ -19,7 +20,7 @@ const Login = () => {
 			console.log('error',error.message)
 		})
 	}
-	console.log(data)
+	
 	const handleLogout=()=>{
 		signOut(auth)
 		.then(result=>{
@@ -30,13 +31,27 @@ const Login = () => {
 			console.log(error)
 		})
 	}
+	const handleGitAuth=()=>{
+		signInWithPopup(auth,gitProvider)
+		.then(result=>{
+			const user=result.user;
+			setData(user)
+		})
+		.catch(error=>{
+			console.log(error)
+		})
+	}
+	console.log(data)
 
 	
 	return (
 		<div>
 			{data?
 			<button onClick={handleLogout}>logout</button>:
+			<>
 			<button onClick={handleGoogle}>Google login</button>
+			<button onClick={handleGitAuth} >Github signin</button>
+			</>
 			}
 
                          {data && <div>
